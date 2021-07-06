78881950f6ff25fe2b301f74f081aaae457fa04d
LeCaToX <70011797+LeCaToX@users.noreply.github.com>
2021-07-06 11:28:24 +0700

    basic code for bot

---
+++ tgboj-bot/index.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");	
+require("dotenv").config();
+const bot = new Discord.Client();
+const pref = "tgb!";
+bot.on('ready', function(){	  
+    console.log("TGBOJ Discord Bot now is online");	
+})	
+bot.on("message", async message =>{
+    if (message.author.bot) return;
+    const text = message.content.toLowerCase();
+    if (text === "ping"){
+        message.channel.send("Xin chào, đây là TGBOt !");
+    }
+    console.log(text);
+    //commands with prefix
+    if (text.startsWith(pref)){
+        const args = text.slice(pref.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
+        console.log(args);
+        if (command === "avatar"){
+            let member = message.mentions.users.first() || message.author
+            let avatar = member.displayAvatarURL({dynamic: true, size: 1024})
+            const embed = new Discord.MessageEmbed()
+                .setTitle(`${member.username}'s avatar`)
+                .setImage(avatar)
+                .setColor("0bb9ee")
+            message.channel.send(embed);
+        }
+        
+
+
+        //help commands
+
+    }
+})
+bot.login(process.env.DISCORDBOT_TOKEN);
